b9648df9b5c2f393d57e489dac199e302d09012b
mero913 <ahmetzyanov.timur@gmail.com>
2021-10-30 16:43:26 +0300

    Create binary search algorithm

---
+++ Binary_search/Binary_search.py
@@ -0,0 +1,24 @@
+# Binary search - by analogy with "phonebook"
+
+def binary_search(list, item):
+
+    low = 0
+    high = len(list) - 1
+
+    while low<=high:
+        mid = (high + low)//2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9]
+my_item = 4
+
+print(binary_search(my_list, my_item))
+                                                                                                                                                          
